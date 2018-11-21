a0ebe852c7da8ab98ca67c4ab2920144979f69e9
Patrick Fox <44874289+pnfox@users.noreply.github.com>
2018-11-21 14:39:49 +0000

    Unused mergesort code

--- terroriser.py
+++ terroriser.py
@@ -47,39 +47,6 @@ def json2points(f):
     # results = [xvalue, yvalue, option1, option2, ...]
     return results
 
-# Python program for implementation of MergeSort
-def mergeSort(arr):
-    if len(arr) >1:
-        mid = len(arr)//2 #Finding the mid of the array
-        L = arr[:mid] # Dividing the array elements
-        R = arr[mid:] # into 2 halves
-
-        mergeSort(L) # Sorting the first half
-        mergeSort(R) # Sorting the second half
-
-        i = j = k = 0
-
-        # Copy data to temp arrays L[] and R[]
-        while i < len(L) and j < len(R):
-            if L[i] < R[j]:
-                arr[k] = L[i]
-                i+=1
-            else:
-                arr[k] = R[j]
-                j+=1
-            k+=1
-
-        # Checking if any element was left
-        while i < len(L):
-            arr[k] = L[i]
-            i+=1
-            k+=1
-
-        while j < len(R):
-            arr[k] = R[j]
-            j+=1
-            k+=1
-
 def order(a, b):
     # need to order a and move b accordingly
     # could map each entry in a with b
@@ -96,7 +63,6 @@ def order(a, b):
         k += 1
 
     return a, b
-
 
 def drawGraph(dataPoints, config):
 
