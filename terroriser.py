3c45888900a0f186fa095a35dae634f56f7bff1b
Patrick Fox <44874289+pnfox@users.noreply.github.com>
2018-11-22 15:14:53 +0000

    If no splits from gui then default is branch

--- terroriser.py
+++ terroriser.py
@@ -51,6 +51,7 @@ def json2points(f):
     global splitNames
     splitNames = list(points[0][2].keys())
     global numOfSplits
+    global splitChoice
     numOfSplits = len(splitChoice)
 
     global axisPos
@@ -225,10 +226,14 @@ def analyseData(url, config):
     global splitChoice
     somID = re.search(r"id=(\d+)", url).group(1)
     splits = re.findall(r'&f_(\w+)=1', url)
-    for i in splits:
-        splitChoice.append(i)
     if splits:
-        config[1] = 1
+        for i in splits:
+            splitChoice.append(i)
+            config[1] = 1
+    # no splits from gui so default splits
+    else:
+        splitChoice = ['branch']
+
     # catch exception that wget fails (or maybe rage unavailable)
     if os.name == "posix":
         os.system("wget -q \"" + str(url)+ "\" -O /tmp/somdata" + str(somID))
