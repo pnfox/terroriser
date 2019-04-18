7c9c4521bbff276bef64714efbb37efa5fb90e58
Patrick Fox <44874289+pnfox@users.noreply.github.com>
2019-04-18 12:32:23 +0100

    Add pointID into data structure of json2points returner Use temp arrays for ordering and plotting x[i] arrays x[i] arrays now have the form [(pointID, xValue0) , (pointID, xValue1), ...]

--- terroriser.py
+++ terroriser.py
@@ -78,8 +78,9 @@ def json2points(f):
     # find split options in json
     j = 0
     for i in points:
-        results.append([i[0], i[1]])
+        results.append([j, i[0], i[1]])
         dict = i[2]
+        # Add the splitByIdentifier, used later to determine color
         if not splitChoice:
             for v in dict.values():
                 results[j].append(v)
@@ -90,7 +91,7 @@ def json2points(f):
     global nPoints
     nPoints = j
 
-    # results = [xvalue, yvalue, option1, option2, ...]
+    # results = [xvalue, yvalue, splitByIdentifier]
     return results
 
 # Used to order two arrays a, b when each entry in both
@@ -99,8 +100,12 @@ def json2points(f):
 # returns ordered arrays a, b
 def order(a, b):
     map = []
-    for i in range(len(a)):
-        map.append([a[i], b[i]])
+    lenA = len(a)
+    if lenA > 1:
+        for i in range(lenA):
+            map.append([a[i], b[i]])
+    else:
+        map.append([a,b])
 
     map = sorted(map)
     k = 0
@@ -142,7 +147,7 @@ def drawGraph(dataPoints, config):
                 if x[plotIndex][p] == pos[0] and y[plotIndex][p] == pos[1]:
                     # FIX: p doesn't point to same point in x and dataPoints
                     # fix with this p = len(x[plotIndex]) + p
-                    text = "x-value: " + str(pos[0]) + "\ny-value: " + str(pos[1]) + "\n" + str(dataPoints[s+p][2])
+                    text = "x-value: " + str(pos[0]) + "\ny-value: " + str(pos[1])
                     break
         else:
             for p in dataPoints:
@@ -182,6 +187,7 @@ def drawGraph(dataPoints, config):
         global axisPos
         for p in dataPoints:
             # collect different x,y's of each split value, excluding xaxis
+            # s will be the str concat of all splitIndentifies from json2points()
             s = ""
             for i in range(numOfSplits):
                 if i in axisPos:
@@ -190,11 +196,11 @@ def drawGraph(dataPoints, config):
                     # then dont continue as we want to color different branches
                     if config[1] == 0:
                         continue
-                s += p[i+2] + "\n"
+                s += p[i+3] + "\n"
             try:
                 position = group.index(s)
-                x[position].append(p[0])
-                y[position].append(p[1])
+                x[position].append((p[0], p[1]))
+                y[position].append((p[0], p[2]))
             except:
                 # didnt match
                 group.append(s)
@@ -202,8 +208,8 @@ def drawGraph(dataPoints, config):
 
     elif numOfSplits == 1:
         for p in dataPoints:
-            x.append(p[0])
-            y.append(p[1])
+            x.append(p[1])
+            y.append(p[2])
 
     global nPoints
     pointSize=250/(nPoints)**0.35
@@ -211,18 +217,28 @@ def drawGraph(dataPoints, config):
     # if we use splits or config[1] then color different plots
     if numOfSplits > 1 or config[1]:
         for i in range(len(x)):
+
+            # order points so that plotted properly
+            # tmpX and tmpY arrays will contain only floating points
+            # of both dependent and indenpendent variables
+            tmpX = []; tmpY = []
+            lenX = len(x[i]); lenY = len(y[i])
+            assert(lenX == lenY)
+            for k in range(lenX):
+                tmpX.append(x[i][k][1])
+                tmpY.append(y[i][k][1])
+            tmpX, tmpY = order(tmpX, tmpY)
+
             if showlegend:
-                # order points so that plotted properly
-                x[i], y[i] = order(x[i], y[i])
                 if config[2] == 1:
-                    sc.append(plt.plot(x[i], y[i], label=group[i]))
+                    sc.append(plt.plot(tmpX, tmpY, label=group[i]))
                 else:
-                    sc.append(plt.scatter(x[i], y[i], label=group[i]))
+                    sc.append(plt.scatter(tmpX, tmpY, label=group[i]))
             else:
                 if config[2] == 1:
-                    sc.append(plt.plot(x[i], y[i]))
+                    sc.append(plt.plot(tmpX, tmpY))
                 else:
-                    sc.append(plt.scatter(x[i], y[i], s=pointSize))
+                    sc.append(plt.scatter(tmpX, tmpY, s=pointSize))
     # no color used
     else:
         if config[2] == 1:
@@ -257,6 +273,8 @@ def analyseData(url, config):
     # no splits from gui so default splits
     else:
         splitChoice = ['branch']
+
+    xaxisChoice = re.findall(r'xaxis=(\w+)', url)
 
     # catch exception that wget fails (or maybe rage unavailable)
     if os.name == "posix":
