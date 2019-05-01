73d1e999231aed8dcc785d0241f502ded6c4d25e
Patrick Fox <44874289+pnfox@users.noreply.github.com>
2019-05-01 11:06:07 -0400

    Initial regression script

---
+++ regression.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import os
+from sklearn.linear_model import LinearRegression
+import terroriser
+
+# Main function for regression analysis
+if __name__=="__main__":
+    
+    url = ""
+    config = [0,0]
+    terroriser.analyseData(url, config)
