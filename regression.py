84db91ee569747f71083440bcd4855ddfa3698be
Patrick Fox <44874289+pnfox@users.noreply.github.com>
2019-05-01 17:02:12 +0100

    Building program skeleton

--- regression.py
+++ regression.py
@@ -7,6 +7,9 @@ import terroriser
 # Main function for regression analysis
 if __name__=="__main__":
     
-    url = ""
+    comparisonBranch = getComparisonBranch()
+    branch = getUserBranch()
+    
+    loginVSImax = "http://rage/?som=512&xaxis=numvms&f_branch=1&v_branch=" + comparisonBranch "&v_branch=" + userBranch
     config = [0,0]
-    terroriser.analyseData(url, config)
+    terroriser.analyseData(loginVSImax, config)
