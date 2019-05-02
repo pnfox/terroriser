535c225f73b2a450dd4a034172bad7ea9814a824
Patrick Fox <44874289+pnfox@users.noreply.github.com>
2019-05-02 15:54:54 +0100

    Demo ML

--- regression.py
+++ regression.py
@@ -3,7 +3,9 @@
 import os
 import argparse
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 from sklearn import svm
 import terroriser
 
@@ -28,12 +30,13 @@ if __name__=="__main__":
     comparisonBranch = cleanString(args.baseline)
     userBranch = cleanString(args.branch)
 
-    loginVSImax = "http://rage/?p=som_data&id=512&xaxis=numvms&f_branch=1&v_branch=" + comparisonBranch + "&v_branch=" + userBranch + "&"
+    loginVSImax = "http://rage/?p=som_data&id=33&xaxis=numvms&f_branch=1&v_branch=" + comparisonBranch + "&v_branch=" + userBranch + "&"
     passmarkCPUScore = "http://rage/?p_som_data&id=562&xaxis=branch&xaxis=build_number&xaxis=build_date&f_branch=1&v_branch=" + comparisonBranch + "&v_branch" + userBranch
     config = [0,0,0]
     try:
         # groupedX takes the form of an array of arrays
         # with each subarray being data of a particular branch
+        print("Getting data...")
         groupedX, groupedY = terroriser.analyseData(loginVSImax, config, True)
         assert(len(groupedX) == len(groupedY))
 
@@ -66,5 +69,37 @@ if __name__=="__main__":
     learningData = np.asarray(x[0])
     targetValues = []
 
+    X = []
+    for i in range(len(x[0])):
+        X.append([x[0][i], y[0][i]])
+
+    X = np.asarray(X)
+    Y = np.dot(X, np.array([1,2])) + 3
+
+    print("Training data...")
+    reg = LinearRegression().fit(X, Y)
+    reg.score(X, Y)
+
+    branchData = []
+    print(len(x[1]))
+    for i in range(len(x[1])):
+        branchData.append([x[1][i], y[1][i]])
+    branchData = np.asarray(branchData)
+    Y = np.dot(branchData, np.array([1,2])) + 3
+
+    print(" Make predictions useing the testing set...")
+    data = reg.predict(branchData)
+    print(data)
+    print(reg.score(branchData, Y))
+
+#    print("Mean squared error: %.2f" % mean_squared_error(y[1], y[0]))
+
 #    classifier.fit(learningData, targetValues)
 
+#    plt.scatter(x[1], Y)
+
+    yvalues = []
+    for i in range(len(data)):
+        yvalues.append(i)
+    plt.scatter(data, yvalues)
+    plt.show()
